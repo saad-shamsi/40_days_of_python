416403681a0bd64cd8f6180b073c279cd3902333
Saad Shamsi <114074040+saad-shamsi@users.noreply.github.com>
2023-10-18 01:06:20 +0500

    added variabes.py in 03_pyton_101

---
+++ 03_python_101/05_variables.py
@@ -0,0 +1,27 @@
+# VARIABLES: variabes are containers that holds information such as str int bool etc..
+
+
+# In Python, variables in a list can be defined using the following rules:
+
+# 1. Variable names must start with a letter or an underscore, and can only contain letters, numbers, and underscores.
+# 2. Variable names cannot be keywords or reserved words in Python.
+# 3. Variable names should be descriptive and easy to understand.
+# 4. Variable names should be in lowercase, with words separated by underscores.
+# 5. Variable names should not be too long or too short.
+# 6. Variable names should be consistent with the naming conventions used in the rest of the program.
+
+# For example, the following are valid variable names:
+
+# my_list
+# list_of_numbers
+# user_input
+
+# The following are invalid variable names:
+
+# 123_list   # Starts with a number
+# my-list    # Contains a hyphen
+# for        # Is a reserved word in Python
+
+
+mang= "magoes"
+print(type(mang))
